7ba90a4a1f83ef4821a536b54949cccde9bcc1b7
kavinKSH <kavin14795@gmail.com>
2023-11-20 12:47:41 +0530

    Initial Commit

---
+++ GitignoreProjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitignoreProjects
+//
+//  Created by Kavin on 20/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
